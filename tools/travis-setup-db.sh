5d954f2147352a30f1e04ed39a234afb170db01a
Kacper Mentel <mentel.kk@gmail.com>
2017-11-09 16:09:44 +0100

    Provide very early Riak SSL configuration
      - use Riak service provided by TravisCI
      - copy configs and certificates into Riak directory
      - setup Riak security

--- tools/travis-setup-db.sh
+++ tools/travis-setup-db.sh
@@ -6,13 +6,15 @@ echo $DB
 
 source tools/travis-common-vars.sh
 
-SQLDIR=${BASE}/apps/ejabberd/priv
+MIM_PRIV_DIR=${BASE}/apps/ejabberd/priv
 
 SQL_TEMP_DIR=/tmp/sql
 
 MYSQL_CONF_DIR=/etc/mysql/conf.d
 
 PGSQL_ODBC_CERT_DIR=~/.postgresql
+
+RIAK_DIR=~/etc/riak
 
 TRAVIS_DB_PASSWORD=$(cat /tmp/travis_db_password)
 
@@ -30,8 +32,8 @@ if [ $DB = 'mysql' ]; then
         -e MYSQL_DATABASE=ejabberd \
         -e MYSQL_USER=ejabberd \
         -e MYSQL_PASSWORD=$TRAVIS_DB_PASSWORD \
-        -v ${SQLDIR}/mysql.cnf:${MYSQL_CONF_DIR}/mysql.cnf:ro \
-        -v ${SQLDIR}/mysql.sql:/docker-entrypoint-initdb.d/mysql.sql:ro \
+        -v ${MIM_PRIV_DIR}/mysql.cnf:${MYSQL_CONF_DIR}/mysql.cnf:ro \
+        -v ${MIM_PRIV_DIR}/mysql.sql:/docker-entrypoint-initdb.d/mysql.sql:ro \
         -v ${BASE}/${TOOLS}/docker-setup-mysql.sh:/docker-entrypoint-initdb.d/docker-setup-mysql.sh \
         -v ${SQL_TEMP_DIR}:${SQL_TEMP_DIR} \
         -p 3306:3306 --name=mongooseim-mysql mysql
@@ -42,9 +44,9 @@ elif [ $DB = 'pgsql' ]; then
     mkdir ${SQL_TEMP_DIR}
     cp ${SSLDIR}/fake_cert.pem ${SQL_TEMP_DIR}/.
     cp ${SSLDIR}/fake_key.pem ${SQL_TEMP_DIR}/.
-    cp ${SQLDIR}/postgresql.conf ${SQL_TEMP_DIR}/.
-    cp ${SQLDIR}/pg_hba.conf ${SQL_TEMP_DIR}/.
-    cp ${SQLDIR}/pg.sql ${SQL_TEMP_DIR}/.
+    cp ${MIM_PRIV_DIR}/postgresql.conf ${SQL_TEMP_DIR}/.
+    cp ${MIM_PRIV_DIR}/pg_hba.conf ${SQL_TEMP_DIR}/.
+    cp ${MIM_PRIV_DIR}/pg.sql ${SQL_TEMP_DIR}/.
     docker run -d \
            -e SQL_TEMP_DIR=${SQL_TEMP_DIR} \
            -e TRAVIS_DB_PASSWORD=${TRAVIS_DB_PASSWORD} \
@@ -68,14 +70,14 @@ ByteaAsLongVarBinary = 1
 EOL
 
 elif [ $DB = 'riak' ]; then
-    # Make riak image with search enabled
-    docker build -t riak $(pwd)/tools/docker/riak
-    docker run -d -p 8087:8087 -p 8098:8098 \
-        -e DOCKER_RIAK_BACKEND=leveldb \
-        -e DOCKER_RIAK_CLUSTER_SIZE=1 \
-        --name=mongooseim-riak riak
-    tools/wait_for_service.sh mongooseim-riak 8098 || docker logs riak
+    ls -l ${RIAKDIR}
+    cat ${RIAKDIR}/riak.conf | grep ssl
+    cp ${SSLDIR}/fake_cert.pem ${RIAK_DIR}/cert.pem
+    cp ${SSLDIR}/fake_key.pem ${RIAK_DIR}/key.pem
+    cp ${SSLDIR}/cacert.pem ${RIAK_DIR}/cacertfile.pem
+    cp ${MIM_PRIV_DIR}/riak-advanced.config ${RIAK_DIR}/advanced.config
     tools/setup_riak
+    riak restart
 
 elif [ $DB = 'cassandra' ]; then
     docker run -d -p 9042:9042 -e MAX_HEAP_SIZE=128M -e HEAP_NEWSIZE=64M --name=cassandra cassandra:${CASSANDRA_VERSION}
